fd3d9b3b331aa8dde08e87665ec80f135a1980d9
CesarCarriel <cesarcarrieldev@gmail.com>
2019-06-05 18:24:49 -0400

    restaurando

--- funcao.js
+++ funcao.js
@@ -1,31 +1,4 @@
-<<<<<<< HEAD
 
-$(document).ready(function(){
-var botao = $("#botao");
-var person = prompt("Player 1");
-
-if (person != null) {
-  document.getElementById("player1").innerHTML =
-  "Player 1:  " + person + "";
-}
- person = prompt("Player 2");
-
-if (person != null) {
-  document.getElementById("player2").innerHTML =
-  "Player 2:  " + person + "";
-}
-
-
-botao.on({
-	click: function(event){
-		alert("aqui");
-	}
-
-});
-
-
-});
-=======
 var cont = 0;
 var contImage= 0;
 
