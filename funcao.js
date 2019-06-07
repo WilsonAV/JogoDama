a2ef66bf785816b2fe81d21d84b0c793e76edf74
Wilson Nunes <wilson-avn1@hotmail.com>
2019-06-06 21:19:15 -0400

    Imagens

--- funcao.js
+++ funcao.js
@@ -1,97 +1,26 @@
+
 $(document).ready(function(){
-	var botao = $("#botao");
-	var img1 = ('<img src = "peca1">');
-	botao.on({
-		click: function(event){
-			event.preventDefault();
-			
-			inserir(this);
-			
-			// $(".preto").css({'background-color' : 'red'});
-			//for (var i = 0; i <12; i++) {
-			//	tabu = $("blue");
+var botao = $("#botao");
+var person = prompt("Player 1");
 
-			
-			}
-		}
-	//})
+if (person != null) {
+  document.getElementById("player1").innerHTML =
+  "Player 1:  " + person + "";
+}
+ person = prompt("Player 2");
 
-,
-		mouseover: function(){
-			botao.css("background-color", "green");
-		},
-		mouseout: function(){
-			botao.css("background-color","");
-		}
+if (person != null) {
+  document.getElementById("player2").innerHTML =
+  "Player 2:  " + person + "";
+}
 
-	});
 
-	function inserir(this){
-		var a = this.val();
-		move ();
-
+botao.on({
+	click: function(event){
+		alert("aqui");
 	}
 
-
-	Calcular();
-	function Calcular(){
-		let preco = $(".info-preco");
-		let qtd = $(".info-qtde");
-		let total= $(".info-total");
-
-		for (var i = preco.length - 1; i >= 0; i--) {
-			total[i].innerHTML =(preco[i].innerHTML*qtd[i].innerHTML).toFixed(2);
-
-		}
-	}
-	Seleciona();
-	function Seleciona(){
-		 $("tr:even").css("background-color", "lightgrey");
-	}
-
-	Valor();
-
-	function Valor(){
-		let preco = $(".info-total");
-		for (var i = preco.length - 1; i >= 0; i--) {
-			if(preco[i].innerHTML >= 50)
-				preco[i].style.color = "red";
-			else if(preco[i].innerHTML < 10)
-				preco[i].style.color = "green";
-			else
-				preco[i].style.color = "orange";
-		}
-	}
-
-	let evento = $(".prod");
-		evento.on("click",function(event){
-			let texto = $(this).text();
-			$("#nome").val(texto);
-					});
-
-
-	function contador(){
-		let preco = $(".info-preco");
-		let qtd = $(".info-qtde");
-		let soma = 0;
-		let soma1 = 0;
-		for (let i = preco.length - 1; i >= 0; i--) {
-			soma += parseFloat(preco[i].innerHTML);		
-		};
-
-		for (let i = qtd.length - 1; i >= 0; i--) {
-			soma1 += parseFloat(qtd[i].innerHTML);		
-		};
-		
-		para.text("Itens : "+preco.length+ " Quantidade: "+soma1 +" Total R$: "+soma);
-
-	}
-
-
-
-
-
-
+});
 
 
 });
